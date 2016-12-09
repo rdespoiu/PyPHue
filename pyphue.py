f15024412ff45dcebb6c43d0da949013385164c9
rdespoiu <roberto.despoiu@gmail.com>
2016-12-09 11:01:45 -0600

    Cleanup, removed some redundancy/added modularity

--- pyphue.py
+++ pyphue.py
@@ -1,10 +1,12 @@
+import sys
 import requests
 import json
-import time
 from pyphueexceptions import *
 
 
 class PyPHue:
+    PYTHON_VERSION = sys.version_info.major
+
     AppName = str()
     DeviceName = str()
     wizard = bool()
@@ -13,82 +15,98 @@ class PyPHue:
     baseURL = str()
     HUE_NUPNP = 'https://www.meethue.com/api/nupnp'
 
-    # Dictionary of friendly light names
-    friendlyLights = dict()
-
     # List of light IDs
     lightIDs = list()
 
-    def __init__(self, ip = None, user = None, AppName = 'My_Hue_App', DeviceName = 'Default_Device JohnDoe', wizard = False):
+
+    ###############
+    # CONSTRUCTOR #
+    ###############
+
+    '''
+    PyPHue can be initialized without any parameters
+
+        IP:         Hue Bridge IP. PyPHue will validate this if used as a parameter.
+
+        User:       Authorized Hue Bridge user. PyPHue will validate this if used as a parameter.
+
+        AppName:    Name of the application, used for generating a user. Default 'My_Hue_App' if left blank.
+
+        DeviceName: Type of device followed by the owner. Default 'iPhone:JohnDoe' if left blank.
+
+        Wizard:     Mostly for debugging. Set wizard to true for verbose bridge connection.
+                    PyPHue will prompt you to press the button on the bridge when creating a user.
+    '''
+
+    def __init__(self, ip = None, user = None, AppName = 'My_Hue_App', DeviceName = 'Default_Device:JohnDoe', wizard = False):
         self.AppName = AppName
         self.DeviceName = DeviceName
         self.wizard = wizard
 
         self.ip = self.validateIP(ip) if ip else self.getBridgeIP()
         self.user = self.validateUser(user) if user else self.createUser()
-        self.baseURL = 'http://{}/api/{}'.format(self.ip, self.user)
+        self.baseURL = '{}/api/{}'.format(self.ip, self.user)
 
         self.mapLights()
 
-    def mapLights(self):
-        lights = self.get('{}/lights/'.format(self.baseURL))
-        self.lightIDs = [lightID for lightID in lights['json'].keys()]
 
-    def assignLightName(self, lightID, name):
-        self.friendlyLights[name] = lightID
+    ###########################
+    # LIGHT STATE CONTROLLERS #
+    ###########################
 
-    def lightInfo(self, lightID):
-        response = self.get('{}/lights/{}/'.format(self.baseURL, lightID))
-        return response
-
-
-    #############################
-    # TURNING LIGHTS ON AND OFF #
-    #############################
-
+    # Flips the state of specified light
     def toggle(self, lightID):
-        state = self.lightInfo(lightID)['json']['state']['on']
-        response = self.put('{}/lights/{}/state/'.format(self.baseURL, lightID), {'on': not state})
+        state = self.getLight(lightID)['json']['state']['on']
+        response = self.putLight(lightID, {'on': not state})
         return response
 
+    # Sets the state of specified light to False (off)
     def turnOff(self, lightID):
-        response = self.put('{}/lights/{}/state/'.format(self.baseURL, lightID), {'on': False})
+        response = self.putLight(lightID, {'on': False})
         return response
 
+    # Sets the state of specified light to True (on)
     def turnOn(self, lightID):
-        response = self.put('{}/lights/{}/state/'.format(self.baseURL, lightID), {'on': True})
+        response = self.putLight(lightID, {'on': True})
         return response
 
 
-    #############################
-    # HUE/BRIGHTNESS/SATURATION #
-    #############################
+    #########################################
+    # HUE/BRIGHTNESS/SATURATION CONTROLLERS #
+    #########################################
 
+    # Brightness: 0 - 254
+    # Saturation: 0 - 254
+    # Hue:        0 - 65535
+
+    # Returns the brightness value of specified light
     def getBrightness(self, lightID):
-        response = self.get('{}/lights/{}/'.format(self.baseURL, lightID))
+        response = self.getLight(lightID)
         return response['json']['state']['bri']
 
-    # Max: 254
+    # Sets the brightness value for specified light
     def setBrightness(self, lightID, brightness):
-        response = self.put('{}/lights/{}/state/'.format(self.baseURL, lightID), {'bri': brightness})
+        response = self.putLight(lightID, {'bri': brightness})
         return response
 
+    # Returns the saturation value of specified light
     def getSaturation(self,lightID):
-        response = self.get('{}/lights/{}/'.format(self.baseURL, lightID))
+        response = self.getLight(lightID)
         return response['json']['state']['sat']
 
-    # Max: 254
+    # Sets the saturation value for specified light
     def setSaturation(self, lightID, saturation):
-        response = self.put('{}/lights/{}/state/'.format(self.baseURL, lightID), {'sat': saturation})
+        response = self.putLight(lightID, {'sat': saturation})
         return response
 
+    # Returns the hue value of spepcified light
     def getHue(self, lightID):
-        response = self.get('{}/lights/{}/'.format(self.baseURL, lightID))
+        response = self.getLight(lightID)
         return response['json']['state']['hue']
 
-    # Max: 65535
+    # Sets the hue value for specified light
     def setHue(self, lightID, hue):
-        response = self.put('{}/lights/{}/state/'.format(self.baseURL, lightID), {'hue': hue})
+        response = self.putLight(lightID, {'hue': hue})
         return response
 
 
@@ -96,38 +114,37 @@ class PyPHue:
     # HTTP METHODS #
     ################
 
+    # GET Request
     def get(self, url):
         response = requests.get(url, timeout = 10)
         return self.responseData(response)
 
+    # PUT Request
     def put(self, url, payload):
         response = requests.put(url, data = json.dumps(payload))
         return self.responseData(response)
 
+    # POST Request
     def post(self, url, payload):
         response = requests.post(url, data = json.dumps(payload))
         return self.responseData(response)
-
-    def responseData(self, response):
-        data = {'status_code': response.status_code, 'ok': response.ok}
-        if response.ok:
-            data['json'] = response.json()
-        return data
 
 
     #############
     # HUE SETUP #
     #############
 
+    # Gets bridge IP using Hue's NUPNP site. Device must be on the same network as the bridge
     def getBridgeIP(self):
         try:
             return self.get(self.HUE_NUPNP)['json'][0]['internalipaddress']
         except:
             raise IPError('Could not resolve Hue Bridge IP address. Please ensure your bridge is connected')
 
+    # If given a brige IP as a constructor parameter, this validates it
     def validateIP(self, ip):
         try:
-            data = requests.get('http://{}/api/'.format(self.ip))
+            data = self.get('http://{}/api/'.format(ip))
             if not data['ok']:
                 raise IPError('Invalid Hue Bridge IP address')
         except (requests.exceptions.ConnectionError,
@@ -135,23 +152,60 @@ class PyPHue:
                 requests.exceptions.ConnectTimeout):
             raise IPError('Invalid Hue Bridge IP address')
 
+        return ip
+
+    # If given a user as a constructor parameter, this validates it
     def validateUser(self, user):
-        response = self.get('http://{}/api/{}'.format(self.ip, user))
+        response = self.get(self.url(self.ip, 'api', user))
 
         if not (type(response['json']) == type(dict()) and response['json'].get('config')):
             raise UserError('Unauthorized user')
 
         return user
 
+    # Creates user. If using wizard (verbose mode), PyPHue will wait for user to press bridge button and manually resume the application
     def createUser(self):
         if self.wizard:
-            print 'Press the button on the Hue Bridge to authenticate'
-            print 'Press any key to continue once you have completed this step'
-            raw_input()
+            print('Press the button on the Hue Bridge to authenticate')
+            print('Press any key to continue once you have completed this step')
+            raw_input() if PYTHON_VERSION < 3 else input()
 
-        response = self.post('http://{}/api/'.format(self.ip), {'devicetype': '{}#{}'.format(self.AppName, self.DeviceName)})
+        response = self.post(self.url(self.ip, 'api'), {'devicetype': '{}#{}'.format(self.AppName, self.DeviceName)})
 
         try:
             return response['json'][0]['success']['username']
         except KeyError:
             raise BridgeError('You must press the button on the Hue Bridge. If you have already done this, ensure your bridge is online and you are both on the same network.')
+
+
+    ###########
+    # HELPERS #
+    ###########
+
+    # URL constructor
+    def url(self, *args):
+        return 'http://{}'.format('/'.join(list(args)))
+
+    # Returns URL for specified light ID
+    def lightURL(self, lightID, state = False):
+        return self.url(self.baseURL, 'lights', lightID) + ('/state' if state else '')
+
+    # PUT a payload at specified light ID
+    def putLight(self, lightID, payload):
+        return self.put(self.lightURL(lightID, True), payload)
+
+    # GET light information
+    def getLight(self, lightID):
+        return self.get(self.lightURL(lightID))
+
+    # Maps lights to local lightIDs array
+    def mapLights(self):
+        lights = self.get('http://{}/lights/'.format(self.baseURL))
+        self.lightIDs = [lightID for lightID in lights['json'].keys()]
+
+    # Takes HTTP request response and returns pertinent information in a dict
+    def responseData(self, response):
+        data = {'status_code': response.status_code, 'ok': response.ok}
+        if response.ok:
+            data['json'] = response.json()
+        return data
