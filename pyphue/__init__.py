2c52e713fc5b80241f7e7be5ca7303b0d141dd11
rdespoiu <roberto.despoiu@gmail.com>
2016-12-09 13:20:28 -0600

    Version 1.0.2

--- pyphue/__init__.py
+++ pyphue/__init__.py
@@ -2,4 +2,5 @@
 # For PyPi Usage #
 ##################
 
+# After installing from PyPi (pip install pyphue), the module can be imported as 'from pyphue import PyPHue' and the actual PyPHue class is imported.
 from .PyPHue import PyPHue
